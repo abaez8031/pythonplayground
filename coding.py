5650c617a8e128ba7ed0c1e279ff906a60e1fdaa
abaez8031 <abaez8031@gmail.com>
2024-07-12 03:16:46 -0400

    caesar

--- coding.py
+++ coding.py
@@ -5,27 +5,43 @@ text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
 
-def encrypt(text, shift):
+# def encrypt(text, shift):
+#   newStr = ""
+#   for letter in text:
+#     if letter not in alphabet:
+#       newStr += letter
+#     else:
+#       oldIdx = alphabet.index(letter)
+#       newStr += alphabet[(oldIdx + shift) % 26]
+#   print(f"The encoded text is {newStr}")
+
+# def decrypt(text, shift):
+#   newStr = ""
+#   for letter in text:
+#     if letter not in alphabet:
+#       newStr += letter
+#     else:
+#       oldIdx = alphabet.index(letter)
+#       newStr += alphabet[(oldIdx - shift)]
+#   print(f"The encoded text is {newStr}")
+
+# if direction == "encode":
+#   encrypt(text,shift)
+# elif direction == "decode":
+#   decrypt(text, shift)
+
+def caesar(text, shift, direction):
   newStr = ""
   for letter in text:
     if letter not in alphabet:
       newStr += letter
     else:
       oldIdx = alphabet.index(letter)
-      newStr += alphabet[(oldIdx + shift) % 26]
+      if direction == "encode":
+        newIdx = (oldIdx + shift) % 26
+      elif direction == "decode":
+        newIdx = oldIdx - shift
+      newStr += alphabet[newIdx]
   print(f"The encoded text is {newStr}")
 
-def decrypt(text, shift):
-  newStr = ""
-  for letter in text:
-    if letter not in alphabet:
-      newStr += letter
-    else:
-      oldIdx = alphabet.index(letter)
-      newStr += alphabet[(oldIdx - shift)]
-  print(f"The encoded text is {newStr}")
-
-if direction == "encode":
-  encrypt(text,shift)
-elif direction == "decode":
-  decrypt(text, shift)
+caesar(text, shift, direction)

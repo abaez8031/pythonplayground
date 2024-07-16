91fd1c70d55addbdfea55febfd71e546cd5009cd
abaez8031 <abaez8031@gmail.com>
2024-07-16 19:07:57 -0400

    turtle

---
+++ snake_game/main.py
@@ -0,0 +1,14 @@
+from turtle import Screen, Turtle
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("My Snake Game")
+positions = [(0,0), (-20,0), (-40,0)]
+
+for position in positions:
+  new_segment = Turtle("square")
+  new_segment.color("white")
+  new_segment.goto(position)
+
+screen.exitonclick()

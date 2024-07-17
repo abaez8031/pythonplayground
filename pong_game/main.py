3d3a5f02b336e66ba74cd73f3366120fc166af70
abaez8031 <abaez8031@gmail.com>
2024-07-17 18:04:12 -0400

    pong

--- pong_game/main.py
+++ pong_game/main.py
@@ -2,7 +2,6 @@ from turtle import Screen
 from paddle import Paddle
 from ball import Ball
 from scoreboard import Scoreboard
-import time
 
 screen = Screen()
 screen.bgcolor("black")
@@ -23,7 +22,6 @@ screen.onkey(l_paddle.go_up, "w")
 
 game_is_on = True
 while game_is_on:
-  time.sleep(0.1)
   screen.update()
   ball.move()
 
@@ -32,15 +30,17 @@ while game_is_on:
     ball.bounce_y()
 
   #Detect collision with the right paddle
-  if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor < -320:
+  if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
     ball.bounce_x()
 
   #Detect right paddle misses
   if ball.xcor() > 380:
     ball.reset_position()
+    scoreboard.l_point()
 
   #Detect left paddle misses
   if ball.xcor() < -380:
     ball.reset_position()
+    scoreboard.r_point()
 
 screen.exitonclick()
--- pong_game/scoreboard.py
+++ pong_game/scoreboard.py
@@ -10,7 +10,21 @@ class Scoreboard(Turtle):
     self.hideturtle()
     self.l_score = 0
     self.r_score = 0
+    self.update_scoreboard()
+
+  def update_scoreboard(self):
     self.goto(-100, 200)
     self.write(self.l_score, align=ALIGNMENT, font=FONT)
     self.goto(100,200)
     self.write(self.r_score, align=ALIGNMENT, font=FONT)
+      
+
+  def l_point(self):
+    self.l_score += 1
+    self.clear()
+    self.update_scoreboard()
+
+  def r_point(self):
+    self.r_score += 1
+    self.clear()
+    self.update_scoreboard()

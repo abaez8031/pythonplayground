d36484dc8593c551f2b9150064192168e101b5a9
abaez8031 <abaez8031@gmail.com>
2024-07-17 17:47:33 -0400

    paddlemisses

--- pong_game/main.py
+++ pong_game/main.py
@@ -1,22 +1,44 @@
 from turtle import Screen
 from paddle import Paddle
+from ball import Ball
+import time
 
 screen = Screen()
 screen.bgcolor("black")
 screen.setup(width=800, height=600)
 screen.title("Pong")
 screen.tracer(0)
-game_is_on = True
 
 r_paddle = Paddle((350,0))
 l_paddle = Paddle((-350, 0))
+ball = Ball()
+
 screen.listen()
 screen.onkey(r_paddle.go_up, "Up")
 screen.onkey(r_paddle.go_down, "Down")
 screen.onkey(l_paddle.go_down, "s")
 screen.onkey(l_paddle.go_up, "w")
 
+game_is_on = True
 while game_is_on:
+  time.sleep(0.1)
   screen.update()
+  ball.move()
+
+  #Detect collision with the top/bottom wall
+  if ball.ycor() > 280 or ball.ycor() < -280:
+    ball.bounce_y()
+
+  #Detect collision with the right paddle
+  if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor < -320:
+    ball.bounce_x()
+
+  #Detect right paddle misses
+  if ball.xcor() > 380:
+    ball.reset_position()
+
+  #Detect left paddle misses
+  if ball.xcor() < -380:
+    ball.reset_position()
 
 screen.exitonclick()

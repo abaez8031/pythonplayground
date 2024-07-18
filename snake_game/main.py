1339c30421a4c2deab3049ef472574585b33400a
abaez8031 <abaez8031@gmail.com>
2024-07-18 01:18:50 -0400

    snakehighscore

--- snake_game/main.py
+++ snake_game/main.py
@@ -34,13 +34,13 @@ while is_on:
 
   #Detect collision with wall
   if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
-    is_on = False
-    scoreboard.game_over()
+    scoreboard.reset()
+    snake.reset()
 
   #Detect collision with tail
   for segment in snake.segments[1:]:
     if snake.head.distance(segment) < 10:
-      scoreboard.game_over()
-      is_on = False
+      scoreboard.reset()
+      snake.reset()
 
 screen.exitonclick()
--- snake_game/snake.py
+++ snake_game/snake.py
@@ -17,7 +17,6 @@ class Snake:
     for position in STARTING_POSTIONS:
       self.add_segment(position)
       
-
   def move(self):
     for seg_num in range(len(self.segments) - 1, 0, -1):
       new_x = self.segments[seg_num - 1].xcor()
@@ -31,6 +30,13 @@ class Snake:
     new_segment.penup()
     new_segment.goto(position)
     self.segments.append(new_segment)
+
+  def reset(self):
+    for segment in self.segments:
+      segment.goto(1000,1000)
+    self.segments.clear()
+    self.create_snake()
+    self.head = self.segments[0]
     
   def extend(self):
     self.add_segment(self.segments[-1].position())

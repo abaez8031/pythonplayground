dbad615f454d9be8f0579ba42f943bfab1a2754f
abaez8031 <abaez8031@gmail.com>
2024-07-18 16:11:15 -0400

    pandas

--- day_25/main.py
+++ day_25/main.py
@@ -2,13 +2,47 @@
 #   data = weather_data.readlines()
 #   print(data)
 
-import csv
+# import csv
+#
+# with open("weather_data.csv") as data_file:
+#     data = csv.reader(data_file)
+#     temperatures = []
+#     for row in data:
+#         if row[1] != "temp":
+#             temperatures.append(int(row[1]))
+# print(temperatures)
 
-with open("weather_data.csv") as data_file:
-  data = csv.reader(data_file)
-  temperatures = []
-  for row in data:
-    if row[1] != "temp":
-      temperatures.append(int(row[1]))
-  print(temperatures)
+import pandas
 
+data = pandas.read_csv("weather_data.csv")
+data_dict = data.to_dict()
+print(data_dict)
+# {
+    # 'day': {
+    # 0: 'Monday',
+    # 1: 'Tuesday',
+    # 2: 'Wednesday',
+    # 3: 'Thursday',
+    # 4: 'Friday',
+    # 5: 'Saturday',
+    # 6: 'Sunday'
+    # },
+    # 'temp': {
+    # 0: 12,
+    # 1: 14,
+    # 2: 15,
+    # 3: 14,
+    # 4: 21,
+    # 5: 22,
+    # 6: 24
+    # },
+    # 'condition': {
+    # 0: 'Sunny',
+    # 1: 'Rain',
+    # 2: 'Rain',
+    # 3: 'Cloudy',
+    # 4: 'Sunny',
+    # 5: 'Sunny',
+    # 6: 'Sunny'
+    # }
+# }
